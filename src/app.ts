5ce8e46df4a0d1c04aaf0e7883bb39df4a10a3ce
firesquid6 <jdeiss06@gmail.com>
2024-12-15 00:05:59 -0600

    add the startApp function

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,34 @@
+import { app } from "@/routes";
+import Elysia from "elysia";
+
+export const kitPlugin = new Elysia()
+  .state("kit", {} as Kit)
+
+export interface Kit {
+  config: Config;
+}
 
 export interface Config {
   port: number;
 }
 
 export function makeConfig(config: Partial<Config>): Config {
+
   return {
     port: config.port ?? 3120,
   }
 }
 
-export function startApp() {
+export function startApp(c: Partial<Config>) {
+  const config = makeConfig(c);
+  const kit: Kit = {
+    config: config,
+  }
+
+  app.store.kit = kit;
+
+  app.listen(config.port, () => {
+    console.log(`App started on ${config.port}`);
+  })
 
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,10 @@
+import { kitPlugin } from "@/app";
 import Elysia from "elysia";
 
 export const app = new Elysia()
+  .use(kitPlugin)
   .get("/", () => {
     return "hello, world!"
   })
+
+export type App = typeof app;
