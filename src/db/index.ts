6fc82d7647449fc66676cb9eda505cca9d456c65
firesquid6 <jdeiss06@gmail.com>
2024-12-15 00:40:29 -0600

    finish the getDb function

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,15 @@
+import { drizzle } from "drizzle-orm/libsql";
 import type { Config } from "@/app";
 
+export type Database = ReturnType<typeof getDb>
+
 export function getDb(config: Config) {
+  return drizzle({
+    connection: {
+      url: config.url,
+      authToken: config.token,
+    }
+
+  })
 
 }
