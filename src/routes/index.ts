3b59d9de71571a110dc5b1467763351bd3337f07
firesquid6 <jdeiss06@gmail.com>
2024-12-15 00:18:51 -0600

    add static directory with elysia plugin

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import type { Kit } from "@/app";
 import { cors } from "@elysiajs/cors";
 import { html } from "@elysiajs/html";
 import { Logestic } from "logestic";
+import staticPlugin from "@elysiajs/static";
 
 export const kitPlugin = new Elysia()
   .state("kit", {} as Kit)
@@ -11,6 +12,10 @@ export const app = new Elysia()
   .use(kitPlugin)
   .use(cors())
   .use(html())
+  .use(staticPlugin({
+    assets: "static",
+    prefix: "/static",
+  }))
   .use(Logestic.preset("common"))
   .get("/", () => {
     return "hello, world!"
