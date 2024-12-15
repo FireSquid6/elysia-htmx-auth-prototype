a9595bee09cdfaf164888022b9111a4fb2d36a97
firesquid6 <jdeiss06@gmail.com>
2024-12-15 00:09:24 -0600

    add a logger

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import Elysia from "elysia";
 import type { Kit } from "@/app";
+import { cors } from "@elysiajs/cors";
+import { Logestic } from "logestic";
 
 export const kitPlugin = new Elysia()
   .state("kit", {} as Kit)
 
 export const app = new Elysia()
   .use(kitPlugin)
+  .use(cors())
+  .use(Logestic.preset("common"))
   .get("/", () => {
     return "hello, world!"
   })
