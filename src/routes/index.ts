2900c5e5ff5acb599ae224bfed837481cfecfd38
firesquid6 <jdeiss06@gmail.com>
2024-12-16 10:28:31 -0600

    add swagger suppor

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,20 @@
 import Elysia from "elysia";
 import { cors } from "@elysiajs/cors";
 import { html } from "@elysiajs/html";
-import { Html } from "@kitajs/html";
 import { Logestic } from "logestic";
 import staticPlugin from "@elysiajs/static";
 import { home } from "./home";
 import { dashboard } from "./dashboard";
 import { auth } from "./auth";
 import { kitPlugin } from "@/app";
+import { swagger } from "@elysiajs/swagger";
 
 // all routes should be consumed here
 export const routes = new Elysia()
   .use(home)
   .use(auth)
   .use(dashboard)
+
 
 export const app = new Elysia()
   .use(kitPlugin())
@@ -23,6 +24,7 @@ export const app = new Elysia()
     assets: "static",
     prefix: "/static",
   }))
+  .use(swagger())
   .use(Logestic.preset("common"))
   .use(routes)
 
